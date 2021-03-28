a95e57bbb64190e2dbb5f7d02c80ad5e36d181e3
deepaknaik04 <20me0123@vvce.ac.in>
2021-03-28 17:11:11 +0000

    2

--- Program-2.c
+++ Program-2.c
@@ -4,15 +4,15 @@
  //Output: 10 20 30
 #include<stdio.h>  
 struct student  
-{  //char name[10];
-    //int id;
+{  char name[10];
+    int id;
     int marks;  
 };  
 int main()  
 {  
   struct student s1,s2,s3;  
   printf("Enter the  marks of student 1,2 and 3");  
-// Read the marks of three students
+scanf("%d%d%d",&s1.marks,&s2.marks,&s3.marks);// Read the marks of three students
  
    printf("%d %d %d",s1.marks,s2.marks,s3.marks); 
   return 0;
